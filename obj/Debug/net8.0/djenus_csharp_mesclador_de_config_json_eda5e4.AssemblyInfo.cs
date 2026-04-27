dcdabcd73fffa6101a840d9d868cebd28996cf92
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 01:06:31 -0300

    feat: entrada CLI mínima
    intent: feature

--- obj/Debug/net8.0/djenus_csharp_mesclador_de_config_json_eda5e4.AssemblyInfo.cs
+++ obj/Debug/net8.0/djenus_csharp_mesclador_de_config_json_eda5e4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("djenus_csharp_mesclador_de_config_json_eda5e4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e030d333ef32680b502261d96829c611a6cb0f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d65d446d9f868c63da4a535fe2b0cd169ca7c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("djenus_csharp_mesclador_de_config_json_eda5e4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("djenus_csharp_mesclador_de_config_json_eda5e4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
