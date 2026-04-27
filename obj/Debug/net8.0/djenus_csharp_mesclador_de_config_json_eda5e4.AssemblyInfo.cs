5b61c64b72f0586900481b4b4686a0545938cd50
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 01:03:22 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/djenus_csharp_mesclador_de_config_json_eda5e4.AssemblyInfo.cs
+++ obj/Debug/net8.0/djenus_csharp_mesclador_de_config_json_eda5e4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("djenus_csharp_mesclador_de_config_json_eda5e4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b5c9ad77381a4ae3a1924b0b2fc85d9597c761")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6e81b1d1663e1a0ea553f04835e82e65144fc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("djenus_csharp_mesclador_de_config_json_eda5e4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("djenus_csharp_mesclador_de_config_json_eda5e4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
